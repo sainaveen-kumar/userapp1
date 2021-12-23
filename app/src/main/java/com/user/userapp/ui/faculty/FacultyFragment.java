ad2b78c563d3ec68e09a5e0a0d5209435edf4b73
sainaveen-kumar <96571496+sainaveen-kumar@users.noreply.github.com>
2021-12-23 16:55:58 +0530

    Initial commit

---
+++ app/src/main/java/com/user/userapp/ui/faculty/FacultyFragment.java
@@ -0,0 +1,24 @@
+package com.user.userapp.ui.faculty;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.user.userapp.R;
+
+
+public class FacultyFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_faculty, container, false);
+    }
+}
